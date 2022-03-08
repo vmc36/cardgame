28b3e2e859c85adce3e24f837dd5e415955fc719
Vinicius <vmcmodesto@gmail.com>
2022-03-07 21:05:24 -0300

    :construction: Create BoardGame without CSS

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
+import BoardGame from "./src/components/BoardGame";
 import CardGame from "./src/components/CardGame";
 
 const $root = document.querySelector("#app");
-const $htmlCardGame = CardGame();
+const $htmlBoardGame = BoardGame(4);
 
-$root.insertAdjacentHTML("beforeend", $htmlCardGame);
+$root.insertAdjacentHTML("beforeend", $htmlBoardGame); /* Insere o elemento na tela */
--- src/components/CardGame/index.js
+++ src/components/CardGame/index.js
@@ -1,16 +1,10 @@
 //JSX
-import "./style.css";
+import "/src/components/styles/style.css"
+
 function CardGame() {
     return `
-        <div class = "card-game-container">
-        <div class = "card-game">
-            <img src= "/src/components/images/caze-1.svg" alt= "cazé-1-image">
-            <img src= "/src/components/images/caze-1.svg" alt= "cazé-1-image">
-            <img src= "/src/components/images/caze-2.svg" alt= "cazé-2-image">
-            <img src= "/src/components/images/caze-2.svg" alt= "cazé-2-image">
-            <img src= "/src/components/images/caze-3.svg" alt= "cazé-3-image">
-            <img src= "/src/components/images/caze-3.svg" alt= "cazé-3-image">
-        </div> 
+        <div class = "interrogacao">
+            <img src= "/src/components/images/interrogacao.svg" alt= "cazé-1-image">
         </div>                
     `;
 
